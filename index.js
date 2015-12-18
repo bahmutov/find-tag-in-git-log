d7c9c0212446aa0b1663050b860131f8ba7efc8a
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-12-17 22:49:17 -0500

    chore(repo): installing

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ const commitSchema = {
 }
 const isCommit = is.schema.bind(null, commitSchema)
 
-function printCommits (l) {
-  console.log('printing %d commit(s)', l.length)
-  console.log(l)
-}
-
 function findTagInCommits (l) {
   var tag
   l.some(commit => {
@@ -29,11 +24,12 @@ function findTagInCommits (l) {
   return tag
 }
 
-function findTag (isTest) {
+function findTagInLog (isTest) {
   // how many commits to consider?
   const N = 5
   return ggit.commits.all(process.cwd())
     .then(commits => commits.slice(0, N))
+    .then(findTagInCommits)
 }
 
-module.exports = findTag
+module.exports = findTagInLog
